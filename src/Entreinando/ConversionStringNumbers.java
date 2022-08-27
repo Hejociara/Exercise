7978101a4596af57062f80d0dbd54b42b4d7bcc3
Hejociara <hejociara@gmail.com>
2022-08-26 21:26:14 -0300

    "Feature"

---
+++ src/Entreinando/ConversionStringNumbers.java
@@ -0,0 +1,20 @@
+package Entreinando;
+
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class ConversionStringNumbers {
+    public static void main(String[] args) {
+        String valor1 = showInputDialog(
+                "Ingresa el primero numero: ");
+        String valor2 = showInputDialog(
+                "Ingresa el segundo número: ");
+
+        System.out.println(valor1 + valor2);
+        double numero1 = Double.parseDouble(valor1);
+        double numero2 = Double.parseDouble(valor2);
+
+        double suma = numero1 + numero2;
+        System.out.println("Suma es " + suma);
+        System.out.println("La media es " + suma /2);
+    }
+}
