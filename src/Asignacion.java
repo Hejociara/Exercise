5a3f4ca4b65802f77aea002d48f1c32d637cf7fa
Hejociara <hejociara@gmail.com>
2022-09-06 19:36:38 -0300

    "Feature"

--- src/Asignacion.java
+++ src/Asignacion.java
@@ -1,5 +1,17 @@
 public class Asignacion {
     public static void main(String[] args) {
-        System.out.println();
+
+        var a = 3;
+        var b = a;
+        var c = a+b;
+
+        c += b;
+        c -= a;
+        c *= b;
+        c /= a;
+
+        System.out.println(c);
+        c %= 2;
+        System.out.println(c);
     }
 }
