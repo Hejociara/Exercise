5c3918290e7f2002d5eb365dfeda96e6d64de62f
Hejociara do Vale <hejociarav@gmail.com>
2022-09-12 19:26:56 -0300

    "Feature"

---
+++ src/Ternary.java
@@ -0,0 +1,16 @@
+public class Ternary {
+    public static void main(String[] args) {
+        double media = 8.6;
+        String resultadoFinal = media >= 7.0 ?
+                "aprobado." : "desaprobado.";
+        System.out.println("El alumno está " + resultadoFinal);
+
+        double nota = 9.8;
+        boolean buenComportamiento = true;
+        boolean pasoPorMedia = nota >= 7.0;
+        boolean trabajo = buenComportamiento && pasoPorMedia;
+        String resultado = trabajo ? "Sí." : "No.";
+
+        System.out.println("Hacer trabajo? " + resultado);
+    }
+}
