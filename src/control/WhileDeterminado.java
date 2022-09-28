d3fe9cb6a12a071d67aa4b19ea00b754f7a08ef7
Hejociara do Vale <hejociarav@gmail.com>
2022-09-28 10:36:32 -0300

    "Feature"

---
+++ src/control/WhileDeterminado.java
@@ -0,0 +1,12 @@
+package control;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 10){
+            System.out.println("Bom dia!");
+            contador = 20;
+        }
+    }
+}
