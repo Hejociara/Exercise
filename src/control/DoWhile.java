0a3fad1481ec025a286f92f576e00086340e42a9
Hejociara <hejociara@gmail.com>
2022-09-30 21:08:31 -0300

    "Feature"

--- src/control/DoWhile.java
+++ src/control/DoWhile.java
@@ -1,4 +1,22 @@
 package control;
 
+import java.util.Scanner;
+
 public class DoWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String texto = "";
+
+        do{
+            System.out.println("Usted necesita hablar " +
+                    "las palabras magicas....");
+            System.out.println("Quiere salir? ");
+            texto = input.nextLine();
+        }
+        while (!texto.equalsIgnoreCase("por favor"));
+
+        System.out.println("Gracias!");
+        input.close();
+    }
 }
