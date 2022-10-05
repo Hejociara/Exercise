03bb3ebff13484f77738e74071a60a99d2a5e130
Hejociara <hejociarav@gmail.com>
2022-10-04 22:29:53 -0300

    "Feature"

--- src/control/SwitchComBreak.java
+++ src/control/SwitchComBreak.java
@@ -15,6 +15,20 @@ public class SwitchComBreak {
             case 10: case 9:
                 conceito = "A";
                 break;
+            case 8: case 7:
+                conceito = "B";
+                break;
+            case 6: case 5:
+                conceito = "C";
+                break;
+            case 4: case 3:
+                conceito = "D";
+                break;
+            case 2: case 1: case 0:
+                conceito = "F";
+                break;
+            default:
+                conceito = "não informado!";
         }
         System.out.println("Conceito é: " + conceito);
         entrada.close();
